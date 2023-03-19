8950b1355fbb0d1f306187c831210fd91c04d93b
Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>
2023-03-19 20:11:13 +0100

    add react router
    Signed-off-by: Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {Fragment} from 'react';
-import {Route, RouterProvider, Routes} from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { RouterProvider } from 'react-router-dom';
 import router from "./router";
 
 const Header = () => <header>Header</header>;
---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import {createBrowserRouter} from "react-router-dom";
+import React from "react";
+
+const Home = () => <h1>Home</h1>;
+const NotFound = () => <h1>Error</h1>;
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    }
+]);
+
+export default router;
