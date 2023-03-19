88fb735185caf4d1f2290deedf10d59fe4e174bd
Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>
2023-03-19 20:51:14 +0100

    add Home component
    Signed-off-by: Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import Card from './components/card/Card';
+import Home from './components/home-page/Home';
 
-const Home = () => (
-  <h1>
-    Home <Card title="title" description="description" image="image"></Card>
-  </h1>
-);
 const NotFound = () => <h1>Error</h1>;
 
 const router = createBrowserRouter([
