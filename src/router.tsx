1a2b59ad08dfd40560adb43152639899e18e63d1
Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>
2023-03-19 20:48:18 +0100

    add Card component
    Signed-off-by: Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,13 @@
-import { createBrowserRouter } from 'react-router-dom';
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
 
-const Home = () => <h1>Home</h1>;
+import Card from './components/card/Card';
+
+const Home = () => (
+  <h1>
+    Home <Card title="title" description="description" image="image"></Card>
+  </h1>
+);
 const NotFound = () => <h1>Error</h1>;
 
 const router = createBrowserRouter([
