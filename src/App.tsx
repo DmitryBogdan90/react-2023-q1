671f75a914161a91ec3b8e1d8791ae4851b68d10
Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>
2023-03-19 20:06:32 +0100

    init project with vite
    Signed-off-by: Dmitry Bogdan <71260124+DmitryBogdan90@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import {Route, RouterProvider, Routes} from 'react-router-dom';
+import router from "./router";
+
+const Header = () => <header>Header</header>;
+
+const App = () => {
+    return (
+        <Fragment>
+            <Header/>
+            <RouterProvider router={router}/>
+        </Fragment>
+    );
+};
+
+export default App;
